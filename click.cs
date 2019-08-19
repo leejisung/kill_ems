5d811aae0930424bc7fe8b0efbf9db487eac31ce
leejisung <starseed13@hanmail.net>
2019-08-19 22:29:48 +0900

    1차 완성

---
+++ click.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+
+public class click : MonoBehaviour
+{
+    public int sss = 0;
+    public GameObject ems;
+    public string keyword = "오함마";
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = ems.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    public void kill()
+    {
+        sss = sss + 1;
+        animator.SetBool("평상", false);
+        animator.SetBool(keyword, true);
+        Invoke("idle", 2);
+
+    }
+    public void idle()
+    {
+        animator.SetBool(keyword, false);
+        animator.SetBool("평상", true);
+    }
+}
